d6b64b94f495058cbbc19597ded8e663761854c6
Claudia Neumann <claudia@Claudias-MacBook-Pro.local>
2018-05-05 12:55:36 +0400

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,25 @@
+$("#special-text").show();
+
+$("#the-movie-button").click(function(){
+	$(".text-container").hide();
+	$("#the-movie-text").show();
+
+
+});
+
+$("#reviews-button").click(function(){
+	$(".text-container").hide();
+	$("#reviews-text").show();
+
+
+});
+
+$("#contact-us-button").click(function(){
+	$(".text-container").hide();
+	$("#contact-us-text").show();
+
+
+
+
+});
+
